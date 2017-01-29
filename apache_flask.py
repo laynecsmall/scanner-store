b02858bfc2e167de3c429ca560951d2bf7377d28
Layne Small <laynecsmall@gmail.com>
2017-01-29 17:05:09 +1300

    added create devie rout logic

--- apache_flask.py
+++ apache_flask.py
@@ -22,13 +22,13 @@ def index():
 
 #-----------------------------------
 #-----------------------------------
-@app.route('/new/result')
+@app.route('/new/result',  methods=['POST'])
 def new_result():
     if request.method == 'POST':
         if sorted(request.form.keys) == sorted(['device_name','time','raw_results']):
             insert_new_result(db, {"device_name": request.form['device_name'],
-                "time":request.form['time'],
-                "raw_results": request.form['raw_results']})
+                                   "time":request.form['time'],
+                                   "raw_results": request.form['raw_results']})
             return make_response(jsonify( { 'success': 'result stored' } ), 200)
         else:
             return make_response(jsonify( { 'failure': 'bad request' } ), 400)
@@ -38,9 +38,19 @@ def new_result():
 
 #-----------------------------------
 #-----------------------------------
-@app.route('/new/device')
+@app.route('/new/device', methods=['POST'])
 def new_device():
-	return render_template('index.html')
+    if request.method == 'POST':
+        if sorted(request.form.keys) == sorted(['device_name','device_type','sensor_x', 'sensor_y']):
+            insert_new_device(db, {"device_name": request.form['device_name'],
+                                   "device_type": request.form['device_type'],
+                                   "sensor_x": request.form['sensor_x'],
+                                   "sensor_y": request.form['sensor_y']})
+            return make_response(jsonify( { 'success': 'device created' } ), 200)
+        else:
+            return make_response(jsonify( { 'failure': 'bad request' } ), 400)
+    else:
+            return make_response(jsonify( { 'failure': 'unsupported method' } ), 400)
 
 #-----------------------------------
 #-----------------------------------
